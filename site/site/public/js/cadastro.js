c349c30564f034d60e0434eb7e831c8333fc2d17
DeividLee <davi.lee@sptech.school>
2023-09-27 16:38:19 -0300

    cadastro e login arrumado

--- site/site/public/js/cadastro.js
+++ site/site/public/js/cadastro.js
@@ -1,18 +1,16 @@
 const wrapper = document.querySelector('.wrapper');
 const loginlink = document.querySelector('.login-link');
 const registerlink = document.querySelector('.register-link');
-// activ que troca
 const btnPopup = document.querySelector('.btnLogin-popup');
 const iconClose = document.querySelector('.icon-close');
 
-registerlink.addEventListener('click', () => {
+loginlink.addEventListener('click', () => {
     wrapper.classList.add('active');
 });
 
-loginlink.addEventListener('click', () => {
+registerlink.addEventListener('click', () => {
     wrapper.classList.remove('active');
 });
-// 
 
 btnPopup.addEventListener('click', () => {
     wrapper.classList.add('active-popup');
