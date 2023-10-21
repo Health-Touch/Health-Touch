38adcbd71abaf9325d382c55c0f4d128ca42daeb
MariaPaulaBS <maria.bsilva@sptech.school>
2023-10-21 00:24:24 -0300

    Erro arrumado

--- site/site/src/routes/ativar.js
+++ site/site/src/routes/ativar.js
@@ -1,7 +1,9 @@
 var express = require("express");
 var router = express.Router();
 
-router.put("/ativar/:id", function (req, res) {
+var ativarController = require("../controllers/ativarController");
+
+router.post("/ativar/:id", function (req, res) {
     ativarController.ativar(req, res);
 });
 
