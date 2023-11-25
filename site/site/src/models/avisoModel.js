a25a05d38f27b592356855164a268789403770a1
Yasmya <yasmin.kakoi@sptech.school>
2023-11-25 09:20:27 -0300

    Filtro das análises

--- site/site/src/models/avisoModel.js
+++ site/site/src/models/avisoModel.js
@@ -1,63 +1,102 @@
-var database = require("../database/config");
+var database = require('../database/config')
 
 function listar() {
-    console.log("ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
-    var instrucao = `
+  console.log(
+    "ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()"
+  )
+  var instrucao = `
         Select idSetor, nome from setor;
-    `;
-    console.log("Executando a instrução SQL: \n" + instrucao);
-    return database.executar(instrucao);
+    `
+  console.log('Executando a instrução SQL: \n' + instrucao)
+  return database.executar(instrucao)
 }
 function filtrar_Parametro() {
-    console.log("ACESSEI O listarStatus \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
-    var instrucao = `
+  console.log(
+    "ACESSEI O listarStatus \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()"
+  )
+  var instrucao = `
         Select idAviso, NivelAviso from aviso ;
-    `;
-    console.log("Executando a instrução SQL: \n" + instrucao);
-    return database.executar(instrucao);
+    `
+  console.log('Executando a instrução SQL: \n' + instrucao)
+  return database.executar(instrucao)
 }
-
 
 function listarTodosFuncionarios() {
-    console.log("ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n");
-    var instrucao = `
+  console.log(
+    "ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n"
+  )
+  var instrucao = `
     select Colaborador.idColaborador, Colaborador.nome, Colaborador.email, StatusColaborador.statusColaborador, NivelAcesso.nivelAcesso from Colaborador join NivelAcesso on idNivelAcesso = fkNivelAcesso
 							join StatusColaborador on idStatusColaborador = fkStatus order by idColaborador;
-    `;
-    console.log("Executando a instrução SQL: \n" + instrucao);
-    return database.executar(instrucao);
+    `
+  console.log('Executando a instrução SQL: \n' + instrucao)
+  return database.executar(instrucao)
 }
 
-
 function verificarSetor(idSetor) {
-    console.log("ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n");
-    var instrucao = `
+  console.log(
+    "ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n"
+  )
+  var instrucao = `
     select count(maquina.idMaquina) as qtdRegistrar from maquina 
                                     join LocalSala on fklocal = idLocalSala
                                     join setor on fksetor = idSetor where setor.idSetor = ${idSetor} ;
-    `;
-    console.log("Executando a instrução SQL: \n" + instrucao);
-    return database.executar(instrucao);
+    `
+  console.log('Executando a instrução SQL: \n' + instrucao)
+  return database.executar(instrucao)
 }
 
 function listarTodosComputadores() {
-    console.log("ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n");
-    var instrucao = `
+  console.log(
+    "ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n"
+  )
+  var instrucao = `
     select  maquina.idMaquina, maquina.so, LocalSala.sala, LocalSala.andar,setor.nome, statusMaquina.statusMaquina from maquina 
     join LocalSala on fklocal = idLocalSala
         join setor on fksetor = idSetor
             join empresa on fkEmpresa = idEmpresa 
             join statusMaquina on fkStatusMaquina = idStatusMaquina order by idMaquina;
-    `;
-    console.log("Executando a instrução SQL: \n" + instrucao);
-    return database.executar(instrucao);
+    `
+  console.log('Executando a instrução SQL: \n' + instrucao)
+  return database.executar(instrucao)
 }
 
+function filtrarComputadores(setor) {
+  console.log(
+    "ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n"
+  )
+  var instrucao = `
+  select  maquina.idMaquina, maquina.so, LocalSala.sala, LocalSala.andar,setor.nome, statusMaquina.statusMaquina from maquina 
+  join LocalSala on fklocal = idLocalSala
+      join setor on fksetor = idSetor
+          join empresa on fkEmpresa = idEmpresa 
+          join statusMaquina on fkStatusMaquina = idStatusMaquina
+          where setor.nome = '${setor}'
+          order by idMaquina;
+    `
+  console.log('Executando a instrução SQL: \n' + instrucao)
+  return database.executar(instrucao)
+}
 
+function filtrarFuncionarios(cargo) {
+  console.log(
+    "ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n"
+  )
+  var instrucao = `
+      select Colaborador.idColaborador, Colaborador.nome, Colaborador.email, StatusColaborador.statusColaborador, NivelAcesso.nivelAcesso from Colaborador join NivelAcesso on idNivelAcesso = fkNivelAcesso
+                              join StatusColaborador on idStatusColaborador = fkStatus
+                              where NivelAcesso.nivelAcesso = '${cargo}'
+                              order by idColaborador;
+      `
+  console.log('Executando a instrução SQL: \n' + instrucao)
+  return database.executar(instrucao)
+}
 
 function listarComputadores(idSetor) {
-    console.log("ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n");
-    var instrucao = `
+  console.log(
+    "ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n"
+  )
+  var instrucao = `
     SELECT 
     maquina.idMaquina,
     LocalSala.sala,
@@ -75,46 +114,52 @@ WHERE
 GROUP BY 
     maquina.idMaquina, LocalSala.sala, LocalSala.andar, setor.nome;
 ;
-    `;
-    console.log("Executando a instrução SQL: \n" + instrucao);
-    return database.executar(instrucao);
+    `
+  console.log('Executando a instrução SQL: \n' + instrucao)
+  return database.executar(instrucao)
 }
 
 function listarUsb() {
-    console.log("ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listarUsb()");
-    var instrucao = `
+  console.log(
+    "ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listarUsb()"
+  )
+  var instrucao = `
     select idUSB, nomeUsb,   DATE_FORMAT(dtHoraInserção, '%m/%d/%Y %H:%i:%s') as dtHoraInsercao, fkMaquina  from USB join Maquina on fkMaquina = idMaquina where fkMaquina = 1;
-    `;
-    console.log("Executando a instrução SQL: \n" + instrucao);
-    return database.executar(instrucao);
+    `
+  console.log('Executando a instrução SQL: \n' + instrucao)
+  return database.executar(instrucao)
 }
 
 function listarConsulta() {
-    console.log("ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listarUsb()");
-    var instrucao = `
+  console.log(
+    "ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listarUsb()"
+  )
+  var instrucao = `
     SELECT count(nomeBotao) as consultaMensal
 FROM analiseToten 
 WHERE MONTH(dataHora) = MONTH((SELECT MAX(dataHora) FROM analiseToten))
   AND YEAR(dataHora) = YEAR((SELECT MAX(dataHora) FROM analiseToten));
-    `;
-    console.log("Executando a instrução SQL: \n" + instrucao);
-    return database.executar(instrucao);
+    `
+  console.log('Executando a instrução SQL: \n' + instrucao)
+  return database.executar(instrucao)
 }
 
 function listarSetor(idMaquina) {
-    console.log("ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listarSetor()");
-    var instrucao = `
+  console.log(
+    "ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listarSetor()"
+  )
+  var instrucao = `
     select idSetor, nome from setor join LocalSala on idSetor = fkSetor join Maquina on fkLocal = idLocalSala where idMaquina = ${idMaquina};
-    `;
-    console.log("Executando a instrução SQL: \n" + instrucao);
-    return database.executar(instrucao);
+    `
+  console.log('Executando a instrução SQL: \n' + instrucao)
+  return database.executar(instrucao)
 }
 
-
-
 function pesquisarDescricao(texto) {
-    console.log("ACESSEI O AVISO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function pesquisarDescricao()");
-    var instrucao = `
+  console.log(
+    "ACESSEI O AVISO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function pesquisarDescricao()"
+  )
+  var instrucao = `
         SELECT 
             a.id AS idAviso,
             a.titulo,
@@ -128,14 +173,16 @@ function pesquisarDescricao(texto) {
             INNER JOIN usuario u
                 ON a.fk_usuario = u.id
         WHERE a.descricao LIKE '${texto}';
-    `;
-    console.log("Executando a instrução SQL: \n" + instrucao);
-    return database.executar(instrucao);
+    `
+  console.log('Executando a instrução SQL: \n' + instrucao)
+  return database.executar(instrucao)
 }
 
 function listarPorUsuario(idUsuario) {
-    console.log("ACESSEI O AVISO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listarPorUsuario()");
-    var instrucao = `
+  console.log(
+    "ACESSEI O AVISO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listarPorUsuario()"
+  )
+  var instrucao = `
         SELECT 
             a.id AS idAviso,
             a.titulo,
@@ -149,51 +196,65 @@ function listarPorUsuario(idUsuario) {
             INNER JOIN usuario u
                 ON a.fk_usuario = u.id
         WHERE u.id = ${idUsuario};
-    `;
-    console.log("Executando a instrução SQL: \n" + instrucao);
-    return database.executar(instrucao);
+    `
+  console.log('Executando a instrução SQL: \n' + instrucao)
+  return database.executar(instrucao)
 }
 
 function publicar(titulo, descricao, idUsuario) {
-    console.log("ACESSEI O AVISO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function publicar(): ", titulo, descricao, idUsuario);
-    var instrucao = `
+  console.log(
+    "ACESSEI O AVISO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function publicar(): ",
+    titulo,
+    descricao,
+    idUsuario
+  )
+  var instrucao = `
         INSERT INTO aviso (titulo, descricao, fk_usuario) VALUES ('${titulo}', '${descricao}', ${idUsuario});
-    `;
-    console.log("Executando a instrução SQL: \n" + instrucao);
-    return database.executar(instrucao);
+    `
+  console.log('Executando a instrução SQL: \n' + instrucao)
+  return database.executar(instrucao)
 }
 
 function editar(novaDescricao, idAviso) {
-    console.log("ACESSEI O AVISO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function editar(): ", novaDescricao, idAviso);
-    var instrucao = `
+  console.log(
+    "ACESSEI O AVISO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function editar(): ",
+    novaDescricao,
+    idAviso
+  )
+  var instrucao = `
         UPDATE aviso SET descricao = '${novaDescricao}' WHERE id = ${idAviso};
-    `;
-    console.log("Executando a instrução SQL: \n" + instrucao);
-    return database.executar(instrucao);
+    `
+  console.log('Executando a instrução SQL: \n' + instrucao)
+  return database.executar(instrucao)
 }
 
 function deletar(idAviso) {
-    console.log("ACESSEI O AVISO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function deletar():", idAviso);
-    var instrucao = `
+  console.log(
+    "ACESSEI O AVISO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function deletar():",
+    idAviso
+  )
+  var instrucao = `
         DELETE FROM aviso WHERE id = ${idAviso};
-    `;
-    console.log("Executando a instrução SQL: \n" + instrucao);
-    return database.executar(instrucao);
+    `
+  console.log('Executando a instrução SQL: \n' + instrucao)
+  return database.executar(instrucao)
 }
 
 module.exports = {
-    listar,
-    listarComputadores,
-    listarTodosComputadores,
-    listarTodosFuncionarios,
-    verificarSetor,
-    listarUsb,
-    listarConsulta,
-    listarSetor,
-    listarPorUsuario,
-    pesquisarDescricao,
-    publicar,
-    editar,
-    deletar,
-    filtrar_Parametro
+  listar,
+  listarComputadores,
+  listarTodosComputadores,
+  listarTodosFuncionarios,
+  verificarSetor,
+  listarUsb,
+  listarConsulta,
+  listarSetor,
+  listarPorUsuario,
+  pesquisarDescricao,
+  publicar,
+  editar,
+  deletar,
+  filtrar_Parametro,
+  filtrarComputadores,
+  filtrarFuncionarios
 }
--- site/site/src/routes/avisos.js
+++ site/site/src/routes/avisos.js
@@ -1,59 +1,67 @@
-var express = require("express");
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
-var avisoController = require("../controllers/avisoController");
+var avisoController = require('../controllers/avisoController')
 
-router.get("/listar", function (req, res) {
-    avisoController.listar(req, res);
-});
-router.get("/filtrar_Parametro", function (req, res) {
-    avisoController.filtrar_Parametro(req, res);
-});
+router.get('/listar', function (req, res) {
+  avisoController.listar(req, res)
+})
+router.get('/filtrar_Parametro', function (req, res) {
+  avisoController.filtrar_Parametro(req, res)
+})
 
-router.get("/listarComputadores/:idSetor", function (req, res) {
-    avisoController.listarComputadores(req, res);
-});
+router.get('/listarComputadores/:idSetor', function (req, res) {
+  avisoController.listarComputadores(req, res)
+})
 
-router.get("/verificarSetor/:idSetor", function (req, res) {
-    avisoController.verificarSetor(req, res);
-});
+router.get('/verificarSetor/:idSetor', function (req, res) {
+  avisoController.verificarSetor(req, res)
+})
 
-router.get("/listarUsb", function (req, res) {
-    avisoController.listarUsb(req, res);
-});
+router.get('/listarUsb', function (req, res) {
+  avisoController.listarUsb(req, res)
+})
 
-router.get("/listarConsulta", function (req, res) {
-    avisoController.listarConsulta(req, res);
-});
-router.get("/listarSetor/:idMaquina", function (req, res) {
-    avisoController.listarSetor(req, res);
-});
+router.get('/listarConsulta', function (req, res) {
+  avisoController.listarConsulta(req, res)
+})
+router.get('/listarSetor/:idMaquina', function (req, res) {
+  avisoController.listarSetor(req, res)
+})
 
-router.get("/listar/:idUsuario", function (req, res) {
-    avisoController.listarPorUsuario(req, res);
-});
+router.get('/listar/:idUsuario', function (req, res) {
+  avisoController.listarPorUsuario(req, res)
+})
 
-router.get("/pesquisar/:descricao", function (req, res) {
-    avisoController.pesquisarDescricao(req, res);
-});
+router.get('/pesquisar/:descricao', function (req, res) {
+  avisoController.pesquisarDescricao(req, res)
+})
 
-router.post("/publicar/:idUsuario", function (req, res) {
-    avisoController.publicar(req, res);
-});
+router.post('/publicar/:idUsuario', function (req, res) {
+  avisoController.publicar(req, res)
+})
 
-router.put("/editar/:idAviso", function (req, res) {
-    avisoController.editar(req, res);
-});
+router.put('/editar/:idAviso', function (req, res) {
+  avisoController.editar(req, res)
+})
 
-router.delete("/deletar/:idAviso", function (req, res) {
-    avisoController.deletar(req, res);
-});
+router.delete('/deletar/:idAviso', function (req, res) {
+  avisoController.deletar(req, res)
+})
 
-router.get("/listarTodosComputadores", function (req, res) {
-    avisoController.listarTodosComputadores(req, res);
-});
+router.get('/listarTodosComputadores', function (req, res) {
+  avisoController.listarTodosComputadores(req, res)
+})
 
-router.get("/listarTodosFuncionarios", function (req, res) {
-    avisoController.listarTodosFuncionarios(req, res);
-});
-module.exports = router;
+router.get('/filtrarComputadores/:setor', function (req, res) {
+  avisoController.filtrarComputadores(req, res)
+})
+
+router.get('/listarTodosFuncionarios', function (req, res) {
+  avisoController.listarTodosFuncionarios(req, res)
+})
+
+router.get('/filtrarFuncionarios/:cargo', function (req, res) {
+  avisoController.filtrarFuncionarios(req, res)
+})
+module.exports = router
